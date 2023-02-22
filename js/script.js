fd3df61f19d5c745524f17a0c71832e81d7dd682
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2023-02-22 18:54:03 +0530

    book house responsive webdesign has completed

--- js/script.js
+++ js/script.js
@@ -31,4 +31,133 @@ window.onload = () =>{
     }else{
         document.querySelector('.header .header-2').classList.remove('active');
     }
+    fadeout();
 }
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeout(){
+    setTimeout(loader,4000);
+}
+
+var swiper = new Swiper(".book-slider", {
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+
+      },
+      1024: {
+        slidesPerView: 3,
+
+      },
+    },
+  });
+
+  var swiper = new Swiper(".featured-slider", {
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+
+      },
+      768: {
+        slidesPerView: 3,
+
+      },
+      1024: {
+        slidesPerView: 4,
+
+      },
+    },
+  });
+
+  var swiper = new Swiper(".arrivals-slider", {
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+
+      },
+      1024: {
+        slidesPerView: 3,
+
+      },
+    },
+  });
+
+  var swiper = new Swiper(".reviews-slider", {
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    grabCursor:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+
+      },
+      1024: {
+        slidesPerView: 3,
+
+      },
+    },
+  });
+
+  var swiper = new Swiper(".blogs-slider", {
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    grabCursor:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+
+      },
+      1024: {
+        slidesPerView: 3,
+
+      },
+    },
+  });
